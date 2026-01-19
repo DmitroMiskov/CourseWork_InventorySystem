44d5fdbf8f4b01998fd576bf629f5e3fec8df3d7
Dmytro Miskov <dima.miskov04@gmail.com>
2026-01-19 22:19:03 +0200

    Fix API Base URL

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,7 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || '';
+const API_URL = import.meta.env.VITE_API_URL; 
 
-axios.defaults.baseURL = baseURL;
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export default axios;
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token'); // або як ви його зберігаєте
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
